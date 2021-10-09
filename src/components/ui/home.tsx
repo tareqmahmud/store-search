61c586e99a377eb69b1e92635268770e3a58c350
MD Tareq Mahmud <tareqmahmud@users.noreply.github.com>
2021-10-09 21:53:56 +0600

    Fix homepage issue

--- src/components/ui/home.tsx
+++ src/components/ui/home.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from '@components/header/header';
 import Hero from '@components/ui/hero';
 import Stores from '@components/ui/stores';
-import Button from '@components/ui/button';
 
 const Home = () => {
   return (
@@ -10,9 +9,6 @@ const Home = () => {
       <Header />
       <Hero />
       <Stores />
-      <div className="mt-12 flex justify-center items-center">
-        <Button label="Load More..." className="rounded-none bg-red-900"/>
-      </div>
     </>
   );
 };
--- src/components/ui/stores.tsx
+++ src/components/ui/stores.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Store from '@components/ui/store';
+import Button from "@components/ui/button";
 
 const Stores = () => {
   return (
-    <div className="container mx-auto">
-      <div className="bg-gray-100 w-full min-h-full py-12 px-12 grid gap-20 grid-cols-4">
-        {Array.from({ length: 12 }).map((_store, index) => (
-          <Store key={index} />
-        ))}
+    <div className="bg-gray-100">
+      <div className="container mx-auto">
+        <div className="w-full min-h-full py-12 px-12 grid gap-20 grid-cols-4">
+          {Array.from({ length: 12 }).map((_store, index) => (
+            <Store key={index} />
+          ))}
+        </div>
+        <div className="flex justify-center">
+          <Button label="Load More..." className="rounded-none bg-red-900 my-5 text-center"/>
+        </div>
       </div>
     </div>
   );
