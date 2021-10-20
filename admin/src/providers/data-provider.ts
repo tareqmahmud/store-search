f2caac634d373c69967ef0945016bb76f2e5476b
MD Tareq Mahmud <tareqmahmud@users.noreply.github.com>
2021-10-20 23:20:05 +0600

    Fix errors

--- admin/src/providers/data-provider.ts
+++ admin/src/providers/data-provider.ts
@@ -1,7 +1,7 @@
 import nestJsCrudDataProvider from "@pankod/refine-nestjsx-crud";
 
 const dataProvider = () => {
-  const API_URL = "http://localhost:3000";
+  const API_URL = process.env.REACT_APP_REST_API_URI as string;
   return nestJsCrudDataProvider(API_URL);
 };
 
